d78f50abf50c430035bc57ea8be02bd9082c505f
Benjamin F <benjaminfalck.home@gmail.com>
2026-01-20 02:34:37 +0200

    added welcome message with verison id

--- BMP-CodeEditor.c
+++ BMP-CodeEditor.c
@@ -5,11 +5,12 @@
 #include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
+#include <string.h>
 
 #define CTRL_KEY(k)  ((k) & 0x1f)
+#define BMPCODEEDITOR_VERSION "0.0.1"
 
-/*** DATA ***/
-
+/**************************************** DATA ****************************************/
 struct editorConfig {
     int screenrows;
     int screencols;
@@ -18,7 +19,7 @@ struct editorConfig {
 
 struct editorConfig E;
 
-/*** TERMINAL ***/
+/************************************** TERMINAL **************************************/
 
 void die(const char *s) {
     write(STDOUT_FILENO, "\x1b[2J", 4);
@@ -57,35 +58,89 @@ char editorReadKey() {
     return c;
 }
 
+int getCursorPosition(int *rows, int *cols) {
+    char buf[32];
+    unsigned int i = 0;
+
+    if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+
+    while (i < sizeof(buf) - 1) {
+        if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
+        if (buf[i] == 'R') break;
+        i++;
+    }
+    buf[i] = '\0';
+
+    if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+    if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
+
+    return 0;
+}
+
 int getWindowSize(int *rows, int *cols) {
     struct winsize ws;
     if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
-        return -1;
+        if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
+        return getCursorPosition(rows, cols);
     } else {
         *cols = ws.ws_col;
         *rows = ws.ws_row;
         return 0;
     }
 }
+/*********************************** APPEND BUFFER ************************************/
+struct abuf {
+    char *b;
+    int len;
+};
 
-/*************************** OUTPUT ***************************/
-void editorDrawRows() {
+#define ABUF_INIT {NULL, 0}
+
+void abAppend(struct abuf *ab, const char *s, int len) {
+    char *new = realloc(ab->b, ab->len + len);
+    if (new == NULL) return;
+    memcpy(&new[ab->len], s, len);
+    ab->b = new;
+    ab->len += len;
+}
+void abFree(struct abuf *ab) {
+    free(ab->b);
+}
+
+/*************************************** OUTPUT ***************************************/
+void editorDrawRows(struct abuf *ab) {
     int y;
     for (y = 0; y < E.screenrows; y++) {
-        write(STDOUT_FILENO, "~\r\n", 3);
+        if (y == E.screenrows / 3) {
+            char welcome[80];
+            int welcomelen = snprintf(welcome, sizeof(welcome),
+                "BMP CodeEditor -- version %s", BMPCODEEDITOR_VERSION);
+            if (welcomelen > E.screencols) welcomelen = E.screencols;
+            abAppend(ab, welcome, welcomelen);
+            } else {
+                abAppend(ab, "~", 1);
+            }
+
+            abAppend(ab, "\x1b[K", 3);
+            if (y < E.screenrows - 1) {
+            abAppend(ab, "\r\n", 2);
+            }
     }
 }
+
 void editorRefreshScreen() {
-    write(STDOUT_FILENO, "\x1b[2J", 4);
-    write(STDOUT_FILENO, "\x1b[H", 3);
-
-    editorDrawRows();
-
-    write(STDOUT_FILENO, "\x1b[H", 3);
+    struct abuf ab = ABUF_INIT;
+    abAppend(&ab, "\x1b[?25l", 6); /*hide cursor*/
+    abAppend(&ab, "\x1b[H", 3);
+    editorDrawRows(&ab);
+    abAppend(&ab, "\x1b[H", 3);
+    abAppend(&ab, "\x1b[?25h", 6); /*show cursor*/
+    write(STDOUT_FILENO, ab.b, ab.len);
+    abFree(&ab);
 }
 
 
-/*** INPUT ***/
+/*************************************** INPUT ****************************************/
 void editorProcessKeypress() {
     char c = editorReadKey();
     switch (c) {
@@ -97,7 +152,7 @@ void editorProcessKeypress() {
     }
 }
 
-/*** INIT ***/
+/**************************************** INIT ****************************************/
 void initEditor() {
     if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
