f72d9ba487f679ae34a2c42a2677d5f1af55ebef
Benjamin F <benjaminfalck.home@gmail.com>
2026-01-20 04:26:45 +0200

    Changed wasd control to arrow controls

--- BMP-CodeEditor.c
+++ BMP-CodeEditor.c
@@ -10,8 +10,15 @@
 #define CTRL_KEY(k)  ((k) & 0x1f)
 #define BMPCODEEDITOR_VERSION "0.0.1"
 
+enum editorKey {
+  ARROW_LEFT = 1000,
+  ARROW_RIGHT,
+  ARROW_UP,
+  ARROW_DOWN
+};
 /**************************************** DATA ****************************************/
 struct editorConfig {
+    int cx, cy;
     int screenrows;
     int screencols;
     struct termios orig_termios;
@@ -49,13 +56,28 @@ void enableRawMode() {
     if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
-char editorReadKey() {
+int editorReadKey() {
     int nread;
     char c;
     while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
         if (nread ==-1 && errno != EAGAIN) die("read");
     }
-    return c;
+    if (c == '\x1b') {
+        char seq[3];
+        if (read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
+        if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
+        if (seq[0] == '[') {
+            switch (seq[1]) {
+                case 'A': return ARROW_UP;
+                case 'B': return ARROW_DOWN;
+                case 'C': return ARROW_RIGHT;
+                case 'D': return ARROW_LEFT;
+            }
+        }
+        return '\x1b';
+    } else {
+        return c;
+    }
 }
 
 int getCursorPosition(int *rows, int *cols) {
@@ -116,9 +138,15 @@ void editorDrawRows(struct abuf *ab) {
             int welcomelen = snprintf(welcome, sizeof(welcome),
                 "BMP CodeEditor -- version %s", BMPCODEEDITOR_VERSION);
             if (welcomelen > E.screencols) welcomelen = E.screencols;
+            int padding = (E.screencols - welcomelen) / 2;
+            if (padding) {
+            abAppend(ab, "#", 1);
+            padding--;
+            }
+            while (padding--) abAppend(ab, " ", 1);
             abAppend(ab, welcome, welcomelen);
             } else {
-                abAppend(ab, "~", 1);
+                abAppend(ab, "#", 1);
             }
 
             abAppend(ab, "\x1b[K", 3);
@@ -133,7 +161,11 @@ void editorRefreshScreen() {
     abAppend(&ab, "\x1b[?25l", 6); /*hide cursor*/
     abAppend(&ab, "\x1b[H", 3);
     editorDrawRows(&ab);
-    abAppend(&ab, "\x1b[H", 3);
+
+    char buf[32];
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+    abAppend(&ab, buf, strlen(buf));
+
     abAppend(&ab, "\x1b[?25h", 6); /*show cursor*/
     write(STDOUT_FILENO, ab.b, ab.len);
     abFree(&ab);
@@ -141,25 +173,51 @@ void editorRefreshScreen() {
 
 
 /*************************************** INPUT ****************************************/
+void editorMoveCursor(int key) {
+    switch (key) {
+        case ARROW_LEFT:
+            E.cx--;
+            break;
+        case ARROW_RIGHT:
+            E.cx++;
+            break;
+        case ARROW_UP:
+            E.cy--;
+            break;
+        case ARROW_DOWN:
+            E.cy++;
+            break;
+    }
+}
+
 void editorProcessKeypress() {
-    char c = editorReadKey();
+    int c = editorReadKey();
     switch (c) {
         case CTRL_KEY('q'):
             write(STDOUT_FILENO, "\x1b[2J", 4);
             write(STDOUT_FILENO, "\x1b[H", 3);
             exit(0);
             break;
+        case ARROW_UP:
+        case ARROW_DOWN:
+        case ARROW_LEFT:
+        case ARROW_RIGHT:
+            editorMoveCursor(c);
+            break;
     }
 }
 
 /**************************************** INIT ****************************************/
 void initEditor() {
+    E.cx = 0;
+    E.cy = 0;
     if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
 
 int main() {
     enableRawMode();
     initEditor();
+    write(STDOUT_FILENO, "\x1b[38;2;255;171;35m", 19); /*SET COLOR TO #FFAB23*/
 
     while (1) {
         editorRefreshScreen();
